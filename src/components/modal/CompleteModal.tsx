1c2a554fcb81f813aaee8385e7d5d27b90dbc92d
bkdragon0228 <rlaqjarb2067@naver.com>
2023-07-29 22:45:42 +0900

    feat : modal 창에 완료한 todo 정보

--- src/components/modal/CompleteModal.tsx
+++ src/components/modal/CompleteModal.tsx
@@ -9,8 +9,13 @@ import styles from './CompleteModal.module.scss';
 const CompleteModal = () => {
   const dispatch = useAppDispatch();
   const isOpen = useAppSelector((state) => state.modal.isOpen);
+  const todo = useAppSelector((state) => state.modal.todo);
 
-  const onClickClose = () => dispatch(closeModal());
+  const onClickClose = () => {
+    if (window.confirm('내용이 저장 안됩니다. 나가시겠습니까?')) {
+      dispatch(closeModal());
+    }
+  };
 
   if (!isOpen) {
     return null;
@@ -22,6 +27,21 @@ const CompleteModal = () => {
         <button className={styles.delete_btn} onClick={onClickClose}>
           X
         </button>
+        <table>
+          <thead>
+            <tr>
+              <th>할 일</th>
+              <th>시간</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{todo.description}</td>
+              <td>시간 입력하기</td>
+            </tr>
+          </tbody>
+        </table>
+        <button className={styles.submit_btn}>확인</button>
       </div>
     </div>
   );
--- src/components/todo/todoContainer.tsx
+++ src/components/todo/todoContainer.tsx
@@ -4,6 +4,8 @@ import uuid from 'react-uuid';
 import Todo from './todo';
 import TodoForm from './todoForm';
 
+import { openModal, setTodo } from '../../store/_reducer/modal';
+
 import { addTodo, deleteTodo, changeDone } from '../../store/_reducer/todo';
 import { useAppSelector, useAppDispatch } from '../../store';
 import { TodoProps } from '../../store/_reducer/todo';
@@ -29,6 +31,18 @@ const TodoContainer = () => {
 
   const handleDelete = async (id: string) => dispatch(deleteTodo(id));
 
+  const handleComplete = (id: string) => {
+    if (window.confirm('완료 목록에 등록하시겠습니까?')) {
+      const current = todos.find((todo) => todo.id === id);
+      if (!current) return;
+
+      dispatch(setTodo(current));
+      dispatch(openModal());
+    }
+
+    dispatch(deleteTodo(id));
+  };
+
   return (
     <div>
       <h2>할 일</h2>
@@ -36,7 +50,7 @@ const TodoContainer = () => {
       <Todo
         todos={todos}
         handleCheckBox={handleCheckBox}
-        handleDelete={handleDelete}
+        handleDelete={handleComplete}
       />
       <CompleteModal />
     </div>
