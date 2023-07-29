3425876a4dab87ff668954210482e3f06958391a
bkdragon0228 <rlaqjarb2067@naver.com>
2023-07-29 16:21:20 +0900

    feat : 명언 api 연결

--- src/components/advice/adviceContainer.tsx
+++ src/components/advice/adviceContainer.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const adviceContainer = () => {
+import axios from 'axios';
+import useFetchData from '../../hooks/useFetchData';
+
+const AdviceContainer = () => {
+  const fetchAdvice = async () => {
+    try {
+      const result = await axios.get('https://api.adviceslip.com/advice');
+      const data = await result.data;
+
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const {
+    data,
+    fetchData: refetchData,
+    isError,
+  } = useFetchData<{
+    slip_id: number;
+    advice: string;
+  }>(fetchAdvice, { advice: '', slip_id: 0 });
+
+  console.log(data);
+
   return <div>명언</div>;
 };
 
-export default adviceContainer;
+export default AdviceContainer;
