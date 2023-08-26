92737c4801a99d57ad058f161c6a425aee3c0e11
bkdragon0228 <rlaqjarb2067@naver.com>
2023-08-26 21:23:12 +0900

    feat : TodoItem molecules component

---
+++ src/components/unit/molecules/Item.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { TodoProps } from '../../../store/_reducer/todo';
+
+import { Box, useStyleConfig } from '@chakra-ui/react';
+
+import CustomButton from '../atoms/Button';
+
+import CheckBox from '../atoms/CheckBox';
+
+type TodoItemProps = TodoProps & {
+  handleCheckBox: (id: string) => void;
+  handleDelete: (id: string, description: string, done: boolean) => void;
+};
+
+const TodoItem: React.FC<TodoItemProps> = ({
+  description,
+  done,
+  id,
+  handleCheckBox,
+  handleDelete,
+}) => {
+  const themes = useStyleConfig('Card');
+
+  return (
+    <Box __css={themes}>
+      <CheckBox
+        isChecked={done}
+        onChange={() => handleCheckBox(id)}
+        style={{ width: '20px' }}
+      />
+      <Box textDecoration={done ? 'line-through' : 'none'}>{description}</Box>
+      <CustomButton
+        onClick={() => handleDelete(id, description, done)}
+        size="sm"
+        width={20}
+      >
+        완료
+      </CustomButton>
+    </Box>
+  );
+};
+
+export default TodoItem;
